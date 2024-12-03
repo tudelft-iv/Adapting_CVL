da351d5db179220bc43610018592864c4aa5a5ee
ZiminXia <xiazimin0221@gmail.com>
2024-12-03 15:24:10 +0100

    add evaluation code

---
+++ dataloader.py
@@ -0,0 +1,165 @@
+import os
+from torch.utils.data import Dataset
+import PIL.Image
+from torchvision import transforms
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import numpy as np
+import random
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+torch.manual_seed(17)
+np.random.seed(0)
+
+transform_grd = transforms.Compose([
+    transforms.Resize([320, 640]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    
+])
+
+transform_sat = transforms.Compose([
+    transforms.Resize([512, 512]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    
+])
+
+class VIGORDataset(Dataset):
+    def __init__(self, root='./datasets/VIGOR', label_root = 'splits_new', transform=None, load_gt=True, known_ori=True):
+        self.root = root
+        self.label_root = label_root
+        self.known_ori = known_ori
+        if transform != None:
+            self.grdimage_transform = transform[0]
+            self.satimage_transform = transform[1]
+            
+        self.city_list = ['SanFrancisco', 'Chicago']
+        # load sat list
+        self.sat_list = []
+        self.sat_index_dict = {}
+
+        idx = 0
+        for city in self.city_list:
+            sat_list_fname = os.path.join(self.root, label_root, city, 'satellite_list.txt')
+            with open(sat_list_fname, 'r') as file:
+                for line in file.readlines():
+                    self.sat_list.append(os.path.join(self.root, city, 'satellite', line.replace('\n', '')))
+                    self.sat_index_dict[line.replace('\n', '')] = idx
+                    idx += 1
+            print('InputData::__init__: load', sat_list_fname, idx)
+        self.sat_list = np.array(self.sat_list)
+        self.sat_data_size = len(self.sat_list)
+        print('Sat loaded, data size:{}'.format(self.sat_data_size))
+
+        # load grd list  
+        self.grd_list = []
+        self.label = []
+        self.sat_cover_dict = {}
+        self.gt_delta = []
+        idx = 0
+        for city in self.city_list:
+            # load grd panorama list
+            label_fname = os.path.join(self.root, self.label_root, city, 'pano_label_balanced.txt')
+                
+            with open(label_fname, 'r') as file:
+                for line in file.readlines():
+                    data = np.array(line.split(' '))
+                    label = []
+                    for i in [1, 4, 7, 10]:
+                        label.append(self.sat_index_dict[data[i]])
+                    label = np.array(label).astype(int)
+                    delta = np.array([data[2:4], data[5:7], data[8:10], data[11:13]]).astype(float)
+                    self.grd_list.append(os.path.join(self.root, city, 'panorama', data[0]))
+                    self.label.append(label)
+                    self.gt_delta.append(delta)
+                    if not label[0] in self.sat_cover_dict:
+                        self.sat_cover_dict[label[0]] = [idx]
+                    else:
+                        self.sat_cover_dict[label[0]].append(idx)
+                    idx += 1
+            print('InputData::__init__: load ', label_fname, idx)
+        self.data_size = len(self.grd_list)
+        print('Grd loaded, data size:{}'.format(self.data_size))
+        self.label = np.array(self.label)
+        self.gt_delta = np.array(self.gt_delta)
+        
+        self.pred_delta = np.zeros(np.shape(self.gt_delta))
+        self.load_gt = load_gt
+        self.predefined_random_rot = None
+    
+    def __len__(self):
+        return self.data_size
+    
+    def __getitem__(self, idx):        
+        # full ground panorama
+        try:
+            grd = PIL.Image.open(os.path.join(self.grd_list[idx]))
+            grd = grd.convert('RGB')
+        except:
+            print('unreadable image')
+            grd = PIL.Image.new('RGB', (320, 640))
+        grd = self.grdimage_transform(grd)
+        # generate a random rotation 
+        if self.known_ori:
+            rotation = 0
+        else:
+            rotation = self.predefined_random_rot[idx]
+
+        grd = torch.roll(grd, (torch.round(torch.as_tensor(rotation)*grd.size()[2]).int()).item(), dims=2)
+                
+        orientation_angle = rotation * 360 # 0 means heading North, counter-clockwise increasing
+        if orientation_angle < 0:
+            orientation_angle += 360
+        # satellite
+        pos_index = 0
+        sat = PIL.Image.open(os.path.join(self.sat_list[self.label[idx][pos_index]]))
+        if self.load_gt == True:
+            [row_offset, col_offset] = self.gt_delta[idx, pos_index] # delta = [delta_lat, delta_lon]
+        else:
+            [row_offset, col_offset] = self.pred_delta[idx, pos_index] # delta = [delta_lat, delta_lon]
+                
+                
+        sat = sat.convert('RGB')
+        width_raw, height_raw = sat.size
+        
+        sat = self.satimage_transform(sat)
+        _, height, width = sat.size()
+        
+        row_offset = np.round(row_offset/height_raw*height)
+        col_offset = np.round(col_offset/width_raw*width)
+        
+        # groundtruth location on the satellite map        
+        # Gaussian GT        
+        gt = np.zeros([1, height, width], dtype=np.float32)
+        gt_with_ori = np.zeros([20, height, width], dtype=np.float32)
+        x, y = np.meshgrid(np.linspace(-width/2+col_offset,width/2+col_offset,width), np.linspace(-height/2-row_offset,height/2-row_offset,height))
+        d = np.sqrt(x*x+y*y)
+        sigma, mu = 4, 0.0
+        gt[0, :, :] = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) )
+        gt = torch.tensor(gt)
+        
+        index = int(orientation_angle // 18)
+        ratio = (orientation_angle % 18) / 18
+        if index == 0:
+            gt_with_ori[0, :, :] = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) ) * (1-ratio)
+            gt_with_ori[19, :, :] = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) ) * ratio
+        else:
+            gt_with_ori[20-index, :, :] = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) ) * (1-ratio)
+            gt_with_ori[20-index-1, :, :] = np.exp(-( (d-mu)**2 / ( 2.0 * sigma**2 ) ) ) * ratio
+        gt_with_ori = torch.tensor(gt_with_ori)
+        
+        orientation = torch.full([2, height, width], np.cos(orientation_angle * np.pi/180))
+        orientation[1,:,:] = np.sin(orientation_angle * np.pi/180)
+        
+        if 'NewYork' in self.grd_list[idx]:
+            city = 'NewYork'
+        elif 'Seattle' in self.grd_list[idx]:
+            city = 'Seattle'
+        elif 'SanFrancisco' in self.grd_list[idx]:
+            city = 'SanFrancisco'
+        elif 'Chicago' in self.grd_list[idx]:
+            city = 'Chicago'
+            
+        return grd, sat, gt, gt_with_ori, orientation, city
---
+++ evaluate_CCVPE.py
@@ -0,0 +1,133 @@
+import os
+import argparse
+from torch.utils.data import DataLoader, Subset
+from torchvision import transforms
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+from dataloader import VIGORDataset, transform_grd, transform_sat
+from models import CVM_VIGOR, CVM_VIGOR_ori_prior
+from losses import infoNCELoss, cross_entropy_loss
+
+
+torch.manual_seed(17)
+np.random.seed(0)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+"The device is: {}".format(device)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--inference_on', choices=('train', 'val', 'test'), default='test')
+parser.add_argument('--pos_only', choices=('True','False'), default='True')
+parser.add_argument('-b', '--batch_size', type=int, help='batch size', default=16)
+parser.add_argument('--known_ori', choices=('True','False'), default='True')
+parser.add_argument('--model', choices=('teacher', 'auxiliary_student', 'final_student'), default=None)
+
+
+
+args = vars(parser.parse_args())
+
+if args['inference_on']=='train': 
+    if args['model'] is None:
+        parser.error("please specify you want to save predictions from 'teacher' or 'auxiliary_student' or 'final_student' model")
+    else:
+        label = args['model'] + '_prediction_on_trainingset'
+
+
+batch_size = args['batch_size']
+inference_on = args['inference_on']
+pos_only = args['pos_only'] == 'True'
+known_ori = args['known_ori'] == 'True'
+
+model_path = '/home/ziminxia/Work/experiments/Adapting_CVL/models/CCVPE/final_student/model.pt'
+dataset_root='/home/ziminxia/Work/datasets/VIGOR'
+
+
+vigor = VIGORDataset(root=dataset_root, transform=(transform_grd, transform_sat), known_ori=known_ori)
+
+with open('shuffled_crossarea_index_list.npy', 'rb') as f:
+    index_list = np.load(f)
+    
+with open('predefined_random_rot.npy', 'rb') as f:
+    predefined_random_rot = np.load(f)
+
+train_indices = index_list[0: int(len(index_list)*0.7)]
+val_indices = index_list[int(len(index_list)*0.7):int(len(index_list)*0.8)]
+test_indices = index_list[int(len(index_list)*0.8):]
+train_set = Subset(vigor, train_indices)
+val_set = Subset(vigor, val_indices)
+test_set = Subset(vigor, test_indices)
+
+if inference_on == 'train':
+    dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=False)
+elif inference_on == 'val':
+    dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+elif inference_on == 'test':
+    dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+vigor.predefined_random_rot = predefined_random_rot
+
+
+torch.cuda.empty_cache()
+
+if known_ori:
+    CVM_model = CVM_VIGOR_ori_prior(device)
+else:
+    CVM_model = CVM_VIGOR(device)
+CVM_model.load_state_dict(torch.load(model_path))
+CVM_model.to(device)
+CVM_model.eval()
+
+distance_in_meters = []
+pred_us = []
+pred_vs = []
+
+for i, data in enumerate(dataloader, 0):
+    with torch.no_grad():
+        print(i)
+        grd, sat, gt, gt_with_ori, gt_orientation, city = data
+        grd = grd.to(device)
+        sat = sat.to(device)
+
+        gt_with_ori = gt_with_ori.to(device)
+
+        gt_flattened = torch.flatten(gt, start_dim=1)
+        gt_flattened = gt_flattened / torch.sum(gt_flattened, dim=1, keepdim=True)
+
+        logits_flattened, heatmap, ori, matching_score_stacked, matching_score_stacked2, matching_score_stacked3, matching_score_stacked4, matching_score_stacked5, matching_score_stacked6 = CVM_model(grd, sat)
+
+        gt = gt.cpu().detach().numpy() 
+        gt_with_ori = gt_with_ori.cpu().detach().numpy() 
+        gt_orientation = gt_orientation.cpu().detach().numpy() 
+        heatmap = heatmap.cpu().detach().numpy()
+        for batch_idx in range(gt.shape[0]):
+            if city[batch_idx] == 'None':
+                pass
+            else:
+                current_gt = gt[batch_idx, :, :, :]
+                loc_gt = np.unravel_index(current_gt.argmax(), current_gt.shape)
+                current_pred = heatmap[batch_idx, :, :, :]
+
+                loc_pred = np.unravel_index(current_pred.argmax(), current_pred.shape)
+
+                pred_us.append(loc_pred[1])
+                pred_vs.append(loc_pred[2])
+
+                pixel_distance = np.sqrt((loc_gt[1]-loc_pred[1])**2+(loc_gt[2]-loc_pred[2])**2)
+                if city[batch_idx] == 'NewYork':
+                    meter_distance = pixel_distance * 0.113248 / 512 * 640
+                elif city[batch_idx] == 'Seattle':
+                     meter_distance = pixel_distance * 0.100817 / 512 * 640
+                elif city[batch_idx] == 'SanFrancisco':
+                    meter_distance = pixel_distance * 0.118141 / 512 * 640
+                elif city[batch_idx] == 'Chicago':
+                    meter_distance = pixel_distance * 0.111262 / 512 * 640
+                distance_in_meters.append(meter_distance) 
+
+
+            
+print(np.mean(distance_in_meters))   
+print(np.median(distance_in_meters))
+
+if inference_on == 'train':
+    scio.savemat(label+'.mat', {'pred_us': np.array(pred_us), 'pred_vs': np.array(pred_vs)})
